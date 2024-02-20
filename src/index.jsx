411823be192ea50af2cdf68f4d8fccc9f4fbe19d
mikaela.verhoosel <mikaela.verhoosel@gmail.com>
2024-02-20 11:59:45 +1100

    moved script into components, controlled component, simple validation

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "semantic-ui-css/semantic.min.css";
 import "./index.css";
-import App from "./App";
 import CalculateCompoundInterest from "./components/CalculateCompoundInterest";
 import reportWebVitals from "./reportWebVitals";
 
